f5430557076bac22f5a26414f15cef1d29b13588
Jonathan MARTIN <j.martin@lonsdale.fr>
2020-12-14 14:24:26 +0100

    js

--- formValidate.js
+++ formValidate.js
@@ -6,46 +6,47 @@
 
 const FormValidate = function(form, onSend){
     const fields = form.querySelectorAll(':required');
-    const mandatory = form.getAttribute("data-mandatory");
+    const mandatory = form.getAttribute('data-mandatory');
     let validity = true;
-    var init = true;
+    let init = true;
 
     this.reset = () => {
         init = true;
         for (let field of fields) {
-            field.classList.remove('valid');
             field.classList.remove('error');
-            field.parentNode.querySelector('.error-msg').innerHTML = '';
+            field.parentNode.querySelector('.field-error-msg').innerHTML = '';
         }
     }
  
-    // check error
     const validate = () => {
         if(init) return;
         validity = true;
         for (let field of fields) {
-            const error_msg = field.parentNode.querySelector('.error-msg');
+            const error_msg = field.parentNode.querySelector('.field-error-msg');
             const dataTypeMismatch = field.dataset.typemismatch;
             const dataPatternMismatch = field.dataset.patternmismatch;
-            const tooLong = field.validity.tooLong;
-            const tooShort = field.validity.tooShort;
             const typeMismatch = field.validity.typeMismatch;
+			  	const tooShort = field.validity.tooShort;
+			  	const tooLong = field.validity.tooLong; 
+			  	const stepMismatch = field.validity.stepMismatch;
             const patternMismatch = field.validity.patternMismatch;
-            const stepMismatch = field.validity.stepMismatch;
             const valueMissing = field.validity.valueMissing;
-
             if (!field.checkValidity()) {
-                field.classList.add('error');
-                field.classList.remove('valid');
-                var msg = "";
-				if((typeMismatch || tooLong || tooShort || stepMismatch) && dataTypeMismatch) msg = dataTypeMismatch;
-				if(patternMismatch && dataPatternMismatch) msg = dataPatternMismatch;
-				if(valueMissing && mandatory) msg = mandatory;
-				field.setCustomValidity(msg);
-				error_msg.innerHTML = field.validationMessage;
-                validity = false;
+					field.classList.add('error');
+					var msg = "";
+					if((typeMismatch || stepMismatch || tooShort || tooLong) && dataTypeMismatch){
+						msg = dataTypeMismatch;
+					}
+					if(patternMismatch && dataPatternMismatch){
+						msg = dataPatternMismatch;
+					}
+					if(valueMissing && mandatory){
+						msg = mandatory;
+					} 
+					field.setCustomValidity(msg);
+					error_msg.innerHTML = field.validationMessage;
+					validity = false;
             } else {
-                field.classList.add('valid');
                 field.classList.remove('error');
                 error_msg.innerHTML = '';
             }
@@ -53,18 +54,22 @@ const FormValidate = function(form, onSend){
         return validity;
     };
 
-    for (let field of fields) {
-        const msg = document.createElement('div');
-        msg.className = 'error-msg';
-        field.parentNode.appendChild(msg);
-        field.addEventListener('input', () => validate());
-    }
+	for (let field of fields) {
+		if(!field.parentNode.querySelector('.field-error-msg')){
+			const msg = document.createElement('div');
+			msg.className = 'field-error-msg';
+			msg.id = field.getAttribute('aria-describedby');
+			field.parentNode.appendChild(msg);
+		}
+		field.addEventListener('input', () => validate());
+		field.addEventListener('change', () => validate());
+	}
 
-    form.onsubmit = e => {
-        e.preventDefault();
-        init = false;
-        validate() && onSend();
-    };
+	form.onsubmit = e => {
+		e.preventDefault();
+		init = false;
+		validate() && onSend();
+	};
 };
 
 export default FormValidate;
